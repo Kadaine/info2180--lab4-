2a3d3dec9504e10043adebebdaac46581a604a52
Kadaine <kadainewilliams@gmail.com>
2017-10-20 17:54:58 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -1,8 +1,10 @@
-
 let borders;
-let condition = false;
+let condition = false; 
+let goal1;
 
 window.onload = function (){
+	goal1 = document.getElementById('end');
+	goal1.onmouseover = message;
 	borders = document.querySelectorAll('.boundary');
 	for (var i = 0; i < borders.length - 1; i++){
 		borders[i].onmouseover = bordercrossed;
@@ -17,4 +19,10 @@ function bordercrossed (){
 			borders[i].className += " youlose";
 	    }
 	}	
+}
+
+function message (){
+	if(!condition){
+		alert("You win the game");
+	}
 }
