0ae6506877fd1c0eb3d198bc4d5943e6bc0e2400
Kadaine <kadainewilliams@gmail.com>
2017-10-20 18:23:45 -0500

    Exercise 4

--- maze.js
+++ maze.js
@@ -1,10 +1,13 @@
 let borders;
-let condition = false; 
+let condition = false;
 let goal1;
+let getstart;
 
 window.onload = function (){
 	goal1 = document.getElementById('end');
 	goal1.onmouseover = message;
+	getstart = document.getElementById('start');
+	getstart.onclick = restart;
 	borders = document.querySelectorAll('.boundary');
 	for (var i = 0; i < borders.length - 1; i++){
 		borders[i].onmouseover = bordercrossed;
@@ -25,4 +28,13 @@ function message (){
 	if(!condition){
 		alert("You win the game");
 	}
+}
+
+function restart(){
+	condition = false;
+	borders = document.querySelectorAll('.boundary');
+	for (var i = 0; i < borders.length - 1; i++){
+		borders[i].className = " boundary";
+	}
+
 }
