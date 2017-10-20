7c518eb823df61c3837c1c346f9598690449cca8
Kadaine <kadainewilliams@gmail.com>
2017-10-20 17:38:56 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,15 +1,20 @@
-let border1;
-let condition = false 
+
+let borders;
+let condition = false;
 
 window.onload = function (){
-	border1 = document.getElementById('boundary1');
-	border1.onmouseover = function(){
-		if(!condition){
-			border1.className += " youlose";
-			condition = true;
-		}
+	borders = document.querySelectorAll('.boundary');
+	for (var i = 0; i < borders.length - 1; i++){
+		borders[i].onmouseover = bordercrossed;
 	}
+}
 
-
-
+function bordercrossed (){
+	if (!condition){
+		condition = true;
+	borders = document.querySelectorAll('.boundary');
+	for (var i = 0; i < borders.length - 1; i++){
+			borders[i].className += " youlose";
+	    }
+	}	
 }
