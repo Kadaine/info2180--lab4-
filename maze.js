934ba066529dccc7007d2f65f600635501c687c5
Kadaine <kadainewilliams@gmail.com>
2017-10-20 19:00:07 -0500

    Exercise 5

--- maze.js
+++ maze.js
@@ -40,7 +40,7 @@ function restart(){
 	condition = false;
 	testwin = false;
 	borders = document.querySelectorAll('.boundary');
-	stat.innerHTML = "Move your mouse over the S to begin";
+	stat.innerHTML = "Move your mouse over the S to begin ";
 	for (var i = 0; i < borders.length - 1; i++){
 		borders[i].className = " boundary";
 	}
